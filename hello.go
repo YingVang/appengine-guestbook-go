84b417edcce7c023da71c4a73bfe7ecf27d00859
Todd McLeod <todd.mcleod@fresnocitycollege.edu>
2015-03-23 14:07:01 -0700

    adds user variables that can be called

--- hello.go
+++ hello.go
@@ -1,37 +1,44 @@
 package hello
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"net/http"
 
-    "appengine"
-    "appengine/user"
+	"appengine"
+	"appengine/user"
 )
 
 func init() {
-    http.HandleFunc("/", handler)
+	http.HandleFunc("/", handler)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    // [START new_context]
-    c := appengine.NewContext(r)
-    // [END new_context]
-    // [START get_current_user]
-    u := user.Current(c)
-    // [END get_current_user]
-    // [START if_user]
-    if u == nil {
-        url, err := user.LoginURL(c, r.URL.String())
-        if err != nil {
-            http.Error(w, err.Error(), http.StatusInternalServerError)
-            return
-        }
-        w.Header().Set("Location", url)
-        w.WriteHeader(http.StatusFound)
-        return
-    }
-    // [END if_user]
-    // [START output]
-    fmt.Fprintf(w, "Hello, %v!", u)
-    // [END output]
+	// [START new_context]
+	c := appengine.NewContext(r)
+	// [END new_context]
+	// [START get_current_user]
+	u := user.Current(c)
+	// [END get_current_user]
+	// [START if_user]
+	if u == nil {
+		url, err := user.LoginURL(c, r.URL.String())
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Location", url)
+		w.WriteHeader(http.StatusFound)
+		return
+	}
+	// [END if_user]
+	// [START output]
+	fmt.Fprintf(w, "Hello, %v!", u)
+	// fmt.Fprintf(w, "%v", u.Email)
+	// fmt.Fprintf(w, "%v", u.AuthDomain)
+	// fmt.Fprintf(w, "%v", u.Admin)
+	// fmt.Fprintf(w, "%v", u.ID)
+	// fmt.Fprintf(w, "%v", u.FederatedIdentity)
+	// fmt.Fprintf(w, "%v", u.FederatedProvider)
+
+	// [END output]
 }
